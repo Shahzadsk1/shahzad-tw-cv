79fc220f61a3ff911a43080261c7981cce49c05c
Shahzadsk1 <shahzad.shaikh2762@gmail.com>
2024-01-15 14:25:23 +0530

    ✅ Responsive for Mobiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "425px",
+        md: "780px",
+        lg: "1024px",
+        xl: "1440px",
+        "2xl": "1920px",
+      },
+    },
   },
   plugins: [],
 };
